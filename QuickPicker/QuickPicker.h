6cf5f5db2dd9e1511ffdca766d1cdf09a05b7766
manuelvrhovac <manuel.vrhovac@icloud.com>
2019-04-10 11:38:51 +0200

    Initial Commit

---
+++ QuickPicker/QuickPicker.h
@@ -0,0 +1,19 @@
+//
+//  QuickPicker.h
+//  QuickPicker
+//
+//  Created by Manuel Vrhovac on 10/04/2019.
+//  Copyright © 2019 Manuel Vrhovac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QuickPicker.
+FOUNDATION_EXPORT double QuickPickerVersionNumber;
+
+//! Project version string for QuickPicker.
+FOUNDATION_EXPORT const unsigned char QuickPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuickPicker/PublicHeader.h>
+
+
