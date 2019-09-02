91db99ca701881e6f2be9d6abdc773c4a070cd62
manuelvrhovac <manuel.vrhovac@icloud.com>
2019-09-02 09:44:03 +0200

    Fixed Package.swift

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "QuickPicker",
-            path: "Sources",
-            dependencies: ["KVFetcher"]
+            dependencies: ["KVFetcher"],
+            path: "Sources"
         )
     ]
 )
