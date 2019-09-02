6234a5117bc6d500eeb603333021752e787ad1ae
manuelvrhovac <manuel.vrhovac@icloud.com>
2019-09-02 09:54:25 +0200

    SPM ios10

--- Package.swift
+++ Package.swift
@@ -4,6 +4,11 @@ import PackageDescription
 
 let package = Package(
     name: "QuickPicker",
+    platforms: [
+        .iOS(.v10),
+        .macOS(.v10_12),
+        .tvOS(.v10)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
